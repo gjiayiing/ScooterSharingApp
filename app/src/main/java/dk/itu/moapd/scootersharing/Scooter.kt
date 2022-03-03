dd19202187d9fdb97a2ead2e912277839f8641d7
gjiayiing <jygoh.2019@sis.smu.edu.sg>
2022-03-03 21:02:41 +0100

    Exercise 2 -redefine class constructors -change to view binding instead of using viewbyid to reference

--- app/src/main/java/dk/itu/moapd/scootersharing/Scooter.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/Scooter.kt
@@ -1,29 +1,14 @@
 package dk.itu.moapd.scootersharing
 
 class Scooter{
-    private var name: String
-    private var where:String
+    private var name: String =""
+    private var where:String =""
 
     constructor(name: String, where: String){
         this.name = name
         this.where = where
     }
-
-    fun getName():String{
-        return name
-    }
-
-    fun setName(name: String){
-        this.name = name
-    }
-
-    fun getWhere(): String{
-        return where
-    }
-
-    fun setWhere(where: String){
-        this.where = where
-    }
+    //removed getter and setters since they could be auto generated
 
     override fun toString(): String {
         return "$name is placed at $where"
--- app/src/main/java/dk/itu/moapd/scootersharing/ScooterSharingActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/ScooterSharingActivity.kt
@@ -2,57 +2,45 @@ package dk.itu.moapd.scootersharing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import java.text.StringCharacterIterator
+import dk.itu.moapd.scootersharing.databinding.ActivityScooterSharingBinding
 
 
 
 class ScooterSharingActivity : AppCompatActivity() {
 
-    //GUI variables
-    private lateinit var lastAddedText: EditText
-    private lateinit var nameText: EditText
-    private lateinit var whereText: EditText
-
-    private val scooter: Scooter = Scooter("","")
-
+    private lateinit var binding: ActivityScooterSharingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scooter_sharing)
+        binding = ActivityScooterSharingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Edit texts
-        lastAddedText = findViewById(R.id.last_added_text)
-        nameText = findViewById(R.id.name_text)
-        whereText = findViewById(R.id.where_text)
+        val lastAddedText = binding.lastAddedText
+        val nameText = binding.nameText
+        val whereText = binding.whereText
 
         //Buttons
 //        addButton = findViewById(R.id.)
-        val addButton = findViewById<Button>(R.id.add_button)
+        val addButton = binding.addButton
         addButton.setOnClickListener {
             if (nameText.text.isNotEmpty() &&
                     whereText.text.isNotEmpty()) {
                 //update the object attributes
                 val name = nameText.text.toString().trim()
                 val where = whereText.text.toString().trim()
-
-                scooter.setName(name)
-                scooter.setWhere(where)
+                val formScooter = Scooter(name, where)
 
                 //Reset the text fields and update the UI
                 lastAddedText.setText("")
                 nameText.setText("")
                 whereText.setText("")
-                updateUI()
-
+                updateUI(formScooter)
             }
 
         }
     }
-
-    private fun updateUI(){
-        lastAddedText.setText(scooter.toString())
+    private fun updateUI(update:Scooter){
+        binding.lastAddedText.setText(update.toString())
     }
 }
