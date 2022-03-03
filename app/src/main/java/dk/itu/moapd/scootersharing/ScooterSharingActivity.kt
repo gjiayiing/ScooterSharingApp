88d172a81421071343823ee06fbbe8abf3fb93c0
gjiayiing <jygoh.2019@sis.smu.edu.sg>
2022-03-03 13:25:53 +0100

    Update: shift scooter class to Scooter.kt

--- app/src/main/java/dk/itu/moapd/scootersharing/ScooterSharingActivity.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/ScooterSharingActivity.kt
@@ -8,35 +8,7 @@ import android.widget.LinearLayout
 import java.text.StringCharacterIterator
 
 
-class Scooter{
-    private var name: String
-    private var where:String
 
-    constructor(name: String, where: String){
-        this.name = name
-        this.where = where
-    }
-
-    fun getName():String{
-        return name
-    }
-
-    fun setName(name: String){
-        this.name = name
-    }
-
-    fun getWhere(): String{
-        return where
-    }
-
-    fun setWhere(where: String){
-        this.where = where
-    }
-
-    override fun toString(): String {
-        return "$name is placed at $where"
-    }
-}
 class ScooterSharingActivity : AppCompatActivity() {
 
     //GUI variables
